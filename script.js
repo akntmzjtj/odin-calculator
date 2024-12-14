4ffccbe244d6cef89bab15ad6a670e4e381ca248
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2024-12-15 00:36:01 +0800

    Added semi working calculator logic. no = spam yet

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
-const numberButtons = document.querySelectorAll(".numbers button");
+const display = document.querySelector("#display");
 let currentDisplay = "";
+let operatorCount = 0;
+let first = "";
+let second = "";
+let currentOperator = "";
 
+const numberButtons = document.querySelectorAll(".numbers button");
 numberButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        const display = document.querySelector("#display");
-        currentDisplay += button.getAttribute("id");
+        if (operatorCount == 0) {
+            first += button.textContent;
+        }
+        else if (operatorCount == 1) {
+            second += button.textContent;
+        }
 
-        display.textContent = currentDisplay;
+        // display
+        display.textContent = first + " " + currentOperator + " " + second;
     })
 
     console.log(button.getAttribute("id"));
 });
+
+const operatorButtons = document.querySelectorAll(".operators button");
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        // store currentOperator
+        if (button.textContent != "=" && operatorCount == 0) {
+            currentOperator = button.textContent;
+            operatorCount++;
+        }
+        else if (button.textContent != "=" && operatorCount == 1) {
+            first = evaluate();
+            second = "";
+            currentOperator = button.textContent;
+        }
+        else if (button.textContent == "=" && operatorCount == 1) {
+            first = evaluate();
+            second = "";
+            currentOperator = "";
+            operatorCount = 0;
+        }
+
+        display.textContent = first + " " + currentOperator + " " + second;
+    })
+
+    console.log(button.getAttribute("id"));
+});
+
+function evaluate() {
+    let answer = -1;
+    switch (currentOperator) {
+        case "+":
+            answer = Number(first) + Number(second);
+            console.log(answer);
+            break;
+        case "-":
+            answer = Number(first) - Number(second);
+            console.log(answer);
+            break;
+        case "*":
+            answer = Number(first) * Number(second);
+            break;
+        case "/":
+            answer = Number(first) / Number(second);
+            break;
+    }
+    return answer;
+}
